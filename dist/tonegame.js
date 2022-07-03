ef890c90c2d56a97571498997eff9d39277b097a
zakaufman <zakakaufman@gmail.com>
2022-07-02 23:31:38 -0500

    three levels implemented

--- dist/tonegame.js
+++ dist/tonegame.js
@@ -1,14 +1,13 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 
 // TODO:
-// Play a different cadance depending on tonic.
-// Allow user to adjust tonic
-// Scoring
-// Finish game when done
+// Allow user to adjust midpoint of range
 // Drop down box for levels
+// Dont score when not singing
 
 const Pitchfinder = require("pitchfinder");
 const { start } = require("tone");
+const DEBUG = false;
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
@@ -16,10 +15,14 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const canvasHeight = 500; //px
 const canvasLeftMargin = 150; //pixels between left of canvas and time=0 line
 const ppms = 0.05; //canvas pixels per ms
-const timePerNote = 1000; //ms
+var timePerNote = 1000; //ms
+if (DEBUG) {
+  timePerNote = 500; //ms
+}
 const timePerRest = 1000; //ms
 const restInterval = 4; //notes between each rest
 const initialRest = 1000; //ms
+const finishRest = 2000; //ms
 const staffColors = ["LimeGreen", "gray", "salmon", "gray", "yellow", "DeepSkyBlue", "gray", "crimson", "gray", "orange", "gray", "violet"];
 const noteColors = ["LimeGreen", "salmon", "yellow", "DeepSkyBlue", "crimson", "orange", "violet"];
 const notePosition = [0, 1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20, 22, 24, 25];
@@ -38,6 +41,7 @@ var analyser = null;
 var mediaStreamSource = null;
 var detectPitch = null;
 var piano = null;
+const noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
 //pitch variables
 const pitchAvgLength = 5;
@@ -45,9 +49,12 @@ var pitchArray = Array(pitchAvgLength).fill(57.0);
 var myPitch = 57.0;
 
 //song variables
-var notes = [8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6, 5, 4, 5, 6, 7, 8]; //8=tonic
-var tonic = 60; //A3=57
+const numNotes = 20;
+var notes = []; //8=tonic
+var tonic = 60; //C4=60
+var userMiddleNote = 55;
 var startTime = null;
+var finishTime = null;
 var time = null;
 var currentNote = 0;
 var prevNote = 0;
@@ -90,15 +97,7 @@ $(document).ready(function () {
   Synth.setVolume(0.5); // set volume [0-1]
 
   $(".stopgame").click(function () {
-    if (stream != null) {
-      stream.getAudioTracks().forEach((track) => {
-        track.stop();
-      });
-      console.log("stopping mic");
-      stream = null;
-      audioContext.close();
-    }
-    stopSong();
+    stopGame();
   });
 
   $(".newgame").click(function () {
@@ -113,6 +112,8 @@ $(document).ready(function () {
 //Song functions
 function startSong() {
   startTime = new Date().getTime();
+  let numRests = Math.floor(numNotes / restInterval);
+  finishTime = initialRest + numRests * timePerRest + numNotes * timePerNote + finishRest;
 }
 function stopSong() {
   startTime = null;
@@ -217,9 +218,13 @@ function renderFrame() {
     gameCanvas.lineTo(canvasLeftMargin, noteScaled);
     gameCanvas.fill();
     gameCanvas.stroke();
+
+    //see if the game is over
+    if (dt > finishTime) {
+      stopGame();
+    }
   }
 }
-
 async function getMedia() {
   if (stream == null) {
     try {
@@ -250,40 +255,145 @@ async function getMedia() {
     }
   }
 }
-
+function stopGame() {
+  $(".stopgame").prop("disabled", true);
+  $(".startgame").prop("disabled", false);
+  $(".newgame").prop("disabled", false);
+  if (stream != null) {
+    stream.getAudioTracks().forEach((track) => {
+      track.stop();
+    });
+    console.log("stopping mic");
+    stream = null;
+    audioContext.close();
+  }
+  stopSong();
+}
 async function startGame(newgame) {
+  $(".startgame").prop("disabled", true);
+  $(".newgame").prop("disabled", true);
+  //reset scoring
+  currentScore = 0;
+  currentProgress = 0;
+  $score.html("--");
+  $progress.html("--");
+
   stopSong();
   await getMedia(); //get the microphone working
-  if (newgame) {
-    //generate new melody
+  if (newgame || notes.length < 1) {
+    let level = parseInt($(".lvlsel").val());
+    console.log(level);
+    genMelody(level); //generate the melody notes
+    console.log(notes);
+    //set the tonic to get midpoint = userMiddleNote
+    let maxNote = Math.max(...notes);
+    let minNote = Math.min(...notes);
+    let midPoint = Math.round((maxNote + minNote) / 2);
+    tonic = userMiddleNote - (notePosition[midPoint] - notePosition[8]);
+    console.log(tonic);
   }
 
-  //play the cadance
-  piano.play("A", 3, 2);
-  piano.play("C#", 4, 2);
-  piano.play("E", 4, 2);
-  setTimeout(function () {
-    piano.play("A", 3, 2);
-    piano.play("D", 4, 2);
-    piano.play("F#", 4, 2);
-  }, 1000);
-  setTimeout(function () {
-    piano.play("G#", 3, 2);
-    piano.play("B", 3, 2);
-    piano.play("E", 4, 2);
-  }, 2000);
-  setTimeout(function () {
-    piano.play("A", 3, 2);
-    piano.play("C#", 4, 2);
-    piano.play("E", 4, 2);
-  }, 3000);
-  setTimeout(function () {
-    piano.play("A", 3, 2);
-  }, 4000);
-  setTimeout(function () {
+  //play the cadence
+  if (!DEBUG) {
+    playCadence();
+    setTimeout(function () {
+      startSong();
+      myAniReq = window.requestAnimationFrame(drawGame);
+      $(".stopgame").prop("disabled", false);
+    }, 5000);
+  } else {
     startSong();
     myAniReq = window.requestAnimationFrame(drawGame);
-  }, 5000);
+    $(".stopgame").prop("disabled", false);
+  }
+}
+
+function genMelody(level) {
+  notes = [];
+  let nextNote = null;
+  let minNote = null;
+  let maxNote = null;
+  let maxStep = null;
+  let up = 0;
+  let count = getRandomInt(1, 8);
+  switch (level) {
+    case 1: //start tonic, single steps, tonic to tonic+8
+      nextNote = 8;
+      up = 1;
+      minNote = 8;
+      maxNote = 15;
+      maxStep = 1;
+      break;
+    case 2: //start tonic, single steps, tonic to tonic-8
+      nextNote = 8;
+      up = 0;
+      minNote = 1;
+      maxNote = 8;
+      maxStep = 1;
+      break;
+    case 3: //start tonic, single steps, tonic-3 to tonic+4
+      nextNote = 8;
+      up = getRandomInt(0, 1);
+      minNote = 5;
+      maxNote = 12;
+      maxStep = 1;
+      count = getRandomInt(1, 5);
+      break;
+  }
+
+  for (let i = 0; i < numNotes; i++) {
+    notes.push(nextNote);
+    if (up == 1) {
+      nextNote = Math.min(maxNote, nextNote + getRandomInt(1, maxStep));
+      count--;
+      if (nextNote == maxNote || count < 1) {
+        count = getRandomInt(1, 5);
+        up = false;
+      }
+    } else {
+      nextNote = Math.max(minNote, nextNote - getRandomInt(1, maxStep));
+      count--;
+      if (nextNote == minNote || count < 1) {
+        count = getRandomInt(1, 5);
+        up = true;
+      }
+    }
+  }
+  function getRandomInt(min, max) {
+    //inclusive of min and max
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  }
+}
+
+function playCadence() {
+  //play the cadance
+  playNote(tonic);
+  playNote(tonic + 4);
+  playNote(tonic + 7);
+  setTimeout(function () {
+    playNote(tonic);
+    playNote(tonic + 5);
+    playNote(tonic + 9);
+  }, 1000);
+  setTimeout(function () {
+    playNote(tonic - 1);
+    playNote(tonic + 2);
+    playNote(tonic + 7);
+  }, 2000);
+  setTimeout(function () {
+    playNote(tonic);
+    playNote(tonic + 4);
+    playNote(tonic + 7);
+  }, 3000);
+  setTimeout(function () {
+    playNote(tonic);
+  }, 4000);
+}
+
+function playNote(noteNum) {
+  let noteName = noteStrings[noteNum % 12];
+  let noteNumber = Math.floor(noteNum / 12) - 1;
+  piano.play(noteName, noteNumber, 2);
 }
 
 function drawStaff() {
@@ -324,21 +434,14 @@ function updatePitch() {
     //do nothing
   } else {
     console.log(pitch);
-    // var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-    // var note = Math.round(noteFromPitch(pitch));
-    // let noteName = noteStrings[note % 12];
-    // let noteNumber = Math.floor(note / 12) - 1;
-    // $noteElem.html(noteName + noteNumber);
-    // $numElem.html(note);
-
     let x = noteNumFromPitch(pitch);
     pitchArray.shift();
     pitchArray.push(x);
   }
 }
 
-// C4 = 60 A3 = 57
 function noteNumFromPitch(frequency) {
+  // C4 = 60 A3 = 57
   var noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
   return noteNum + 69;
 }
