c03c2695fbf4b8a62ef84ef2bb09fcf1f28bf56f
zakaufman <zakakaufman@gmail.com>
2022-07-10 13:48:59 -0500

    removed exact from usermedia constraints

--- dist/tonegame.js
+++ dist/tonegame.js
@@ -744,13 +744,12 @@ async function getMedia() {
       //initialize the audiocontext
       var AudioContext = window.AudioContext || window.webkitAudioContext;
       audioContext = new AudioContext();
-      unlockAudioContext(audioContext);
 
       stream = await navigator.mediaDevices.getUserMedia({
         audio: {
-          autoGainControl: { exact: false },
-          noiseSuppression: { exact: false },
-          echoCancellation: { exact: false },
+          autoGainControl: false,
+          noiseSuppression: false,
+          echoCancellation: false,
         },
         video: false,
       });
@@ -785,18 +784,18 @@ async function getMedia() {
       return false;
     }
   }
-  function unlockAudioContext(audioCtx) {
-    if (audioCtx.state !== "suspended") return;
-    const b = document.body;
-    const events = ["touchstart", "touchend", "mousedown", "keydown"];
-    events.forEach((e) => b.addEventListener(e, unlock, false));
-    function unlock() {
-      audioCtx.resume().then(clean);
-    }
-    function clean() {
-      events.forEach((e) => b.removeEventListener(e, unlock));
-    }
-  }
+  // function unlockAudioContext(audioCtx) {
+  //   if (audioCtx.state !== "suspended") return;
+  //   const b = document.body;
+  //   const events = ["touchstart", "touchend", "mousedown", "keydown"];
+  //   events.forEach((e) => b.addEventListener(e, unlock, false));
+  //   function unlock() {
+  //     audioCtx.resume().then(clean);
+  //   }
+  //   function clean() {
+  //     events.forEach((e) => b.removeEventListener(e, unlock));
+  //   }
+  // }
 }
 
 function hideTabs() {
